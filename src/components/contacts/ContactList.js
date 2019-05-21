d5a35101805310877c6bedee7ba31b1d8eb20e38
Ericko Yap <the.ericko.mail@gmailcom>
2019-05-21 20:18:40 +0700

    Updated package.json to heroku production

--- src/components/contacts/ContactList.js
+++ src/components/contacts/ContactList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ContactDelete from './ContactDelete';
 
 class ContactList extends React.Component {
